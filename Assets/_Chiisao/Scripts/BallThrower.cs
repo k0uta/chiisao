1457a609ee900d3c6259895768be54fc45ac7068
Murilo Costa <murilo.fioravanti.costa@gmail.com>
2019-07-19 09:23:35 -0300

    (#1) Add ball throwing script

---
+++ Assets/_Chiisao/Scripts/BallThrower.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class BallThrower : MonoBehaviour
+{
+    [SerializeField] private GameObject ballPrefab;
+
+    [SerializeField] private Transform startingPosition;
+
+    [SerializeField] private float throwForce;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var ball = Instantiate(ballPrefab);
+            ball.transform.position = startingPosition.position;
+
+            ball.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, throwForce), ForceMode.Impulse);
+        }
+    }
+}
