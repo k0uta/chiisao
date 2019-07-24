1a72f139177c1a240d7a8cd83e4a927415545a04
Murilo Costa <murilo.fioravanti.costa@gmail.com>
2019-07-24 09:43:55 -0300

    Add ball receive behaviour

---
+++ Assets/_Chiisao/Scripts/BallReceiver.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+
+public class BallReceiver : MonoBehaviour
+{
+    private GameObject targetBall = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (IsBallInRange())
+            {
+                BallHit();
+            }
+            else
+            {
+                BallMiss();
+            }
+        }
+    }
+
+    private void BallHit()
+    {
+        Debug.Log("Hit");
+        throw new NotImplementedException();
+    }
+
+    private void BallMiss()
+    {
+        Debug.Log("Miss");
+        throw new NotImplementedException();
+    }
+
+    private bool IsBallInRange()
+    {
+        return targetBall != null;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Ball") && targetBall == null)
+        {
+            targetBall = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag.Equals("Ball") && targetBall == other.gameObject)
+        {
+            targetBall = null;
+        }
+    }
+}
